6f3beaacaad1fc4805ba43368a3e1a6bff111123
ahmednasser1911 <ahmednasser.fcis@gmail.com>
2022-12-10 16:24:32 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CollectionsPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
