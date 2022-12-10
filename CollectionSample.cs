2f764c85f7375e5fa4a75a22a567084f4be032da
ahmednasser1911 <ahmednasser.fcis@gmail.com>
2022-12-10 22:18:50 +0200

    ;fkjv

---
+++ CollectionSample.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsPractice
+{
+    public static class CollectionSample
+    {
+        public static void Queue()
+        {
+            var q = new Queue();
+            q.Enqueue("First item");
+            q.Enqueue("Secound item");
+
+            string? item = null;
+
+            while ((item = (string?)q.Dequeue()) != null)
+            {
+                Console.WriteLine(item);
+                if (q.Count <= 0) break;
+
+            }
+        }
+
+        public static void Stack()
+        {
+            var q = new Stack();
+            q.Push("First item");
+            q.Push("Secound item");
+
+            string? item = null;
+
+            while ((item = (string?)q.Pop()) != null)
+            {
+                Console.WriteLine(item);
+                if (q.Count <= 0) break;
+
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace CollectionsPractice
+﻿using System.Collections;
+
+namespace CollectionsPractice
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            CollectionSample.Queue();
+            CollectionSample.Stack();
+            return;
+            string[] strings = new string[2];
+            strings[0] = "First";
+            strings[1] = "Secound";
+
+            Console.WriteLine("string : {0}, {1}" ,strings);
+
+            var arrayList = new ArrayList(2);
+            arrayList.Add("First");
+
+            arrayList.AddRange(new string[] { "Secound", "third", "Fourth" });
+
+            Console.WriteLine(arrayList.Count);
+
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
